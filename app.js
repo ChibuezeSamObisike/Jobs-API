c8257439f10cee792526c3bf5119465b76bc4517
ChibuezeSamObisike <samobisike@gmail.com>
2024-01-01 19:39:58 +0100

    clean repository

--- app.js
+++ app.js
@@ -2,6 +2,13 @@
 require("dotenv").config();
 require("express-async-errors");
 const express = require("express");
+
+//Extra security packages
+const helmet = require("helmet");
+const cors = require("cors");
+const xss = require("xss-clean");
+const rateLimiter = require("express-rate-limit");
+
 const app = express();
 
 // error handler
@@ -18,8 +25,18 @@ const auth = require("./middleware/authentication");
 const authRouter = require("./routes/auth");
 const jobsRouter = require("./routes/jobs");
 
+app.set("trust proxy", 1);
+
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000, //15 minutes
+    max: 100, // limit each IP to 100 requests per minute
+  })
+);
 app.use(express.json());
-// extra packages
+app.use(helmet());
+app.use(cors());
+app.use(xss());
 
 // routes
 
